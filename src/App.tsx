46828d541bf48c2674a69837277deb77752dab0a
carlosaw <carlosfreevg@gmail.com>
2023-08-10 21:26:57 -0400

    Pesquisa

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,24 @@
 const App = () => {
+  
+
+  const handleSearch = () => {
+    const formPesquisa = document.querySelector("form");
+    
+    if(formPesquisa){
+      formPesquisa.onsubmit = (e) => {
+        e.preventDefault();
+        const pesquisa = e.target.pesquisa.value;
+        if(pesquisa == "") {
+          alert('Digite o nome do filme');
+          return;
+        }
+        fetch(`https://www.omdbapi.com/?i=${pesquisa}$apikey=${apyKey}`);
+      }
+    } else {
+      alert('Não achou');
+    }
+  }    
+  
   // const [movies, setMovies] = useState<Movie[]>([]);
 
   // useEffect(() => {
@@ -18,25 +38,29 @@ const App = () => {
   return (
     <div className="container mx-auto">
       <h1 className="bg-blue-400 text-center pb-2 font-semibold rounded-md">Filmes</h1>
-      <form className="mt-6 p-3 border border-cyan-400 rounded-md flex">
-        <input 
-          id="pesquisa" 
+      <form id="form" className="mt-6 p-3 border border-cyan-400 rounded-md flex">
+        <input
+          type="text"
+          name="pesquisa" 
           placeholder="Digite o nome do filme"
           className="p-2 outline-none flex-1 rounded-md" />
-        <button className="border border-cyan-400 ml-4">Pesquisar</button>
+        <button onClick={handleSearch} className="border border-cyan-400 ml-4">Pesquisar</button>
       </form>
 
-      <div className="lista mt-4 bg-green-500">
+      <div className="lista mt-4">
         <div className="flex gap-4 justify-center">
-          <div className="flex flex-col items-center w-60 text-center bg-red-600 p-4">
-            <img src="https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="" width={200} />
+          <div className="flex flex-col items-center w-60 text-center border border-cyan-400
+          hover:border-cyan-600 p-4">
+            <img src="https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX600.jpg" alt="" width={200} />
             <h2 className="text-xl text-center">Batman Begins</h2>
           </div>
-          <div className="flex flex-col items-center w-60 text-center bg-red-600 p-4">
+          <div className="flex flex-col items-center w-60 text-center border border-cyan-400
+          hover:border-cyan-600 p-4">
             <img src="https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="" width={200} />  
             <h2 className="text-xl">Batman v Superman: Dawn of Justice</h2>
           </div>
-          <div className="flex flex-col items-center w-60 text-center bg-red-600 p-4">
+          <div className="flex flex-col items-center w-60 text-center border border-cyan-400
+          hover:border-cyan-600 p-4">
             <img src="https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="" width={200} />  
             <h2 className="text-xl">Batman v Superman: Dawn of Justice</h2>
           </div>
