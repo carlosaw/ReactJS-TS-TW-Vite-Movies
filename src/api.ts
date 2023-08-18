e6146dce94127a0abe1844aec0628a01da106f70
carlosaw <carlosfreevg@gmail.com>
2023-08-18 11:00:52 -0400

    Com axios

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,28 @@
-const BASE = 'https://jsonplaceholder.typicode.com';
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com'
+});
 
 export const api = {
   getAllPosts: async () => {
-    let response = await fetch(`${BASE}/posts`);
-    let json = await response.json();
-    return json;
+    let response = await http.get('/posts');
+    return response.data;
+    // let response = await fetch(`${BASE}/posts`);
+    // let json = await response.json();
+    // return json;
   }, 
   addNewPost: async (title: string, body: string, userId: number) => {
-    let response = await fetch(`${BASE}/posts`, {
-      method:'POST',
-      body: JSON.stringify({ title, body, userId }),
-      headers: { 'Content-Type' : 'application/json' }
+    let response = await http.post('/posts', {
+      title, body, userId
     });
-    let json = await response.json();
-    return json;
+    return response.data;
+    // let response = await fetch(`${BASE}/posts`, {
+    //   method:'POST',
+    //   body: JSON.stringify({ title, body, userId }),
+    //   headers: { 'Content-Type' : 'application/json' }
+    // });
+    // let json = await response.json();
+    // return json;
   }
 }
